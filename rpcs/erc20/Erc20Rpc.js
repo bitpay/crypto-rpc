38d021d2ced0319ade364f41bd2b8b4e3834f380
Micah Riggan <micahriggan@gmail.com>
2019-03-26 15:34:12 -0400

    Removing scaling aspects of erc20 send

--- rpcs/erc20/Erc20Rpc.js
+++ rpcs/erc20/Erc20Rpc.js
@@ -10,35 +10,10 @@ class Erc20RPC extends EthRPC {
 
   // this will only work on ERC20 tokens with decimals
   async sendToAddress({ address, amount, passphrase}) {
-    const amountStr = amount.toString();
-    const decimalIndex = amountStr.indexOf('.');
-
-    let precision = 0;
-    if (decimalIndex >= 0) {
-      // if there is a decimal, determine the number of decimal places
-      precision = amountStr.length - (decimalIndex + 1);
-    }
-
-    const decimals = await this.erc20Contract.methods.decimals().call();
-    if (precision > decimals) {
-      throw new Error('Precision provided is greater than the ERC20 precision');
-    }
-    if (precision > 20) {
-      throw new Error('Precision provided is too high for this library');
-    }
-
-    const decimalsBN = this.web3.utils.toBN(decimals - precision);
-    const TEN = this.web3.utils.toBN(10);
-
-    // This line is why we can't handle a shift amount > 20
-    // BigNum can't handle scientific notation,
-    // or floats, so we must convert amount into an integer
-    const bigNumAmount = this.web3.utils.toBN(Math.floor(amount * Math.pow(10, precision)));
-    const scaledAmount = bigNumAmount.mul(TEN.pow(decimalsBN)).toString();
     const gasPrice = await this.estimateGasPrice();
     const account = await this.getAccount();
     const contractData = this.erc20Contract.methods
-      .transfer(address, scaledAmount).encodeABI();
+      .transfer(address, amount).encodeABI();
     return this.web3.eth.personal
       .sendTransaction({ from: account, gasPrice, data: contractData, to: this.tokenContractAddress },
         passphrase, (err, result) => {
