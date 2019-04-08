83142f5d79a3777e160b1c2400f5f0081faebf8c
Justin Kook <jkook@bitpay.com>
2019-04-08 10:52:01 -0400

    moved chai to dev dependency, added estimateFee test

--- tests/eth.js
+++ tests/eth.js
@@ -4,7 +4,6 @@ const mocha = require('mocha');
 const { before, describe, it } = mocha;
 const EthereumTx = require('ethereumjs-tx');
 const util = require('web3-utils');
-const EthRPC = require('../rpcs/eth/EthRpc');
 const config = {
   chain: 'ETH',
   host: 'ganache',
@@ -26,7 +25,6 @@ describe('ETH Tests', function() {
   const currency = 'ETH';
   const currencyConfig = config.currencyConfig;
   const rpcs = new CryptoRpc(config, currencyConfig);
-  const ethRPC = new EthRPC(config);
   let txid = '';
   let blockHash = '';
 
@@ -38,12 +36,7 @@ describe('ETH Tests', function() {
 
   it('should estimate fee', async () => {
     const fee = await rpcs.estimateFee({ currency, nBlocks: 4 });
-    assert.isDefined(fee);
-  });
-
-  it('should estimate gas price', async () => {
-    const gasPrice = await ethRPC.estimateGasPrice();
-    assert.isDefined(gasPrice);
+    assert.isTrue(fee === 20000000000);
   });
 
   it('should send raw transaction', async () => {
