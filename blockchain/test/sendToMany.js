1493c8bba595b862ed95430d069970f573350c5f
Micah Riggan <micahriggan@gmail.com>
2020-05-04 17:07:48 -0400

    Updating test to use some addresses, rather than assuming local accounts array exists

--- blockchain/test/sendToMany.js
+++ blockchain/test/sendToMany.js
@@ -1,6 +1,11 @@
 const SendToMany = artifacts.require('SendToMany');
 const CryptoErc20 = artifacts.require('CryptoErc20');
 const ZERO_ADDR = '0x0000000000000000000000000000000000000000';
+const receivers = [
+  '0x52bc44d5378309ee2abf1539bf71de1b7d7be3b5',
+  '0x06b8c5883ec71bc3f4b332081519f23834c8706e',
+  '0x5a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c'
+]
 contract('SendToMany', (accounts) => {
   it('should exist', async() => {
     const batcher = await SendToMany.deployed();
@@ -9,17 +14,18 @@ contract('SendToMany', (accounts) => {
 
   it('should send ether', async() => {
     const batcher = await SendToMany.deployed();
-    const receivers = accounts.slice(1);
+    const balances = await Promise.all(receivers.map( r => web3.eth.getBalance(r)));
     const amounts = new Array(receivers.length).fill(1e18.toString());
-    const balanceBefore = await web3.eth.getBalance(accounts[0]);;
-    console.log('Token balance before', balanceBefore.toString());
+    const balanceBefore = await web3.eth.getBalance(accounts[0]);
     const sum = (1e18*receivers.length).toString();
     await batcher.sendMany(receivers, amounts, ZERO_ADDR, {value: sum});
-    const balanceAfter = await web3.eth.getBalance(accounts[0]);;
-    console.log('ETH balance after', balanceAfter.toString());
-    for(const receiver of receivers) {
+    const balanceAfter = await web3.eth.getBalance(accounts[0]);
+    assert(balanceBefore > balanceAfter, "Account zero should have lower balance");
+    for(let i = 0; i < receivers.length; i++) {
+      const receiver = receivers[i];
+      const balanceBefore = balances[i];
       const balance = await web3.eth.getBalance(receiver);
-      console.log('ETH Balance', receiver, ':',  balance.toString());
+      assert(balance > balanceBefore, "Balance should increase");
     }
   });
 
@@ -31,18 +37,16 @@ contract('SendToMany', (accounts) => {
   it('should send tokens', async() => {
     const batcher = await SendToMany.deployed();
     const token = await CryptoErc20.deployed();
-    const receivers = accounts.slice(1);
+    const balances = await Promise.all(receivers.map( r => token.balanceOf(r)));
     const amounts = new Array(receivers.length).fill(1e18.toString());
     const sum = (1e18*receivers.length).toString();
-    const balanceBefore = await token.balanceOf(accounts[0]);
-    console.log('Token balance before', balanceBefore.toString());
     await token.approve(batcher.address, sum);
     await batcher.sendMany(receivers, amounts, token.address);
-    const balanceAfter = await token.balanceOf(accounts[0]);
-    console.log('Token balance after', balanceAfter.toString());
-    for(const receiver of receivers) {
+    for(let i = 0; i < receivers.length; i++) {
+      const receiver = receivers[i];
+      const balanceBefore = balances[i];
       const balance = await token.balanceOf(receiver);
-      console.log('Token Balance', receiver, ':',  balance.toString());
+      assert(balance > balanceBefore, "Balance should increase");
     }
   });
 
@@ -50,20 +54,29 @@ contract('SendToMany', (accounts) => {
   it('should send many different tokens', async() => {
     const batcher = await SendToMany.deployed();
     const token = await CryptoErc20.deployed();
-    const receivers = accounts.slice(1);
+    const balances = await Promise.all(receivers.map( r => token.balanceOf(r)));
     const amounts = new Array(receivers.length).fill(1e18.toString());
     const sum = (1e18*receivers.length).toString();
-    const balanceBefore = await token.balanceOf(accounts[0]);
-    console.log('Token balance before', balanceBefore.toString());
     await token.approve(batcher.address, sum);
     const tokens = new Array(receivers.length).fill(token.address)
-    await batcher.batchSend(receivers, amounts, tokens);
-    const balanceAfter = await token.balanceOf(accounts[0]);
-    console.log('Token balance after', balanceAfter.toString());
-    for(const receiver of receivers) {
-      const balance = await token.balanceOf(receiver);
-      console.log('Token Balance', receiver, ':',  balance.toString());
+
+    //send one ETH
+    tokens[0] = ZERO_ADDR;
+    balances[0] = await web3.eth.getBalance(receivers[0]);
+
+
+    await batcher.batchSend(receivers, amounts, tokens, {value: 1e18.toString()});
+    for(let i = 0; i < receivers.length; i++) {
+      const tokenAddress = tokens[i];
+      const receiver = receivers[i];
+      const balanceBefore = balances[i];
+      const ethBalance = await web3.eth.getBalance(receiver);
+      const tokenBalance = await token.balanceOf(receiver);
+      if(tokenAddress != ZERO_ADDR) {
+        assert(tokenBalance > balanceBefore, "Balance should increase");
+      } else {
+        assert(ethBalance > balanceBefore, "Balance should increase");
+      }
     }
   });
-
 });
