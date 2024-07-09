740d3a0283869e393378755f922208da983cd9e2
lyambo <lyambo@bitpay.com>
2024-07-08 22:39:25 -0400

    Solana RPC

--- tests/docker/solana/start-solana.sh
+++ tests/docker/solana/start-solana.sh
@@ -4,7 +4,7 @@
 solana-test-validator --reset --ledger /solana/data > solana-validator.log & echo "Starting Solana"
 
 # Wait for the validator to start
-sleep 10
+sleep 5
 
 # Extract the public key from the keypair file
 export PUBLIC_KEY=$(solana-keygen pubkey /solana/keypair/id.json)
@@ -14,7 +14,7 @@ export PUBLIC_KEY2=$(solana-keygen pubkey /solana/keypair/id2.json)
 solana airdrop 100 $PUBLIC_KEY --url localhost
 echo "Public Key1: $PUBLIC_KEY"
 solana airdrop 100 $PUBLIC_KEY2 --url localhost
-echo "Public Key2: $PUBLIC_KEY"
+echo "Public Key2: $PUBLIC_KEY2"
 
 # Tail the logs
 tail -f solana-validator.log
